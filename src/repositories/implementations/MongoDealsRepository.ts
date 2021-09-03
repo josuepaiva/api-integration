08b562a55fb81fe6306ad9ce6fca4a94f8629d2b
Josue Paiva <josuepaiva@lavid.ufpb.br>
2021-09-03 16:25:39 -0300

    método agregation implementado

--- src/repositories/implementations/MongoDealsRepository.ts
+++ src/repositories/implementations/MongoDealsRepository.ts
@@ -3,9 +3,17 @@ import Deal, { DealDocument } from '../../models/deal.schema';
 import { DealDTO } from '../../models/deal.dto';
 
 export class MongoDealsRepository implements IDealsRepository {
-  agregation(): Promise<[DealDocument]>{
-    // DealSchema.aggregate([{}])
-    return
+  async agregation(): Promise<any>{
+    const docs = await Deal.aggregate([
+      {
+        $group: {
+          _id: '$date',
+          total: { $sum: '$valor'},
+        }
+      }
+    ]);
+
+    return docs;
   }
   async save(deal: DealDTO): Promise<DealDocument> {
     const res = await Deal.create(deal);
